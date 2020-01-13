afe682a537f3aff2b54156faa40b9d55e6f65d97
Vitalik Kim <kimv1086@gmail.com>
2020-01-13 16:10:22 +0600

    added lb68

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Layout from "./components/Layout/Layout";
+import Checkout from "./containers/Checkout/Checkout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Orders from "./containers/Orders/Orders";
+import Counter from "./containers/Counter/Counter";
+import ToDo from "./containers/ToDo/ToDo";
+
+const App = () => (
+
+    <Layout>
+      <Switch>
+        <Route path="/" exact component={BurgerBuilder} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/counter" exact component={Counter} />
+        <Route path="/todo" exact component={ToDo} />
+        <Route render={() => <h1>Not found</h1>} />
+      </Switch>
+    </Layout>
+
+);
 
 export default App;
